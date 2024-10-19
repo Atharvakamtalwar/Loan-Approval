2b5a309b12405f75c48e5b938d8856393b169d5e
Atharvakamtalwar <atharvakamtalwar@gmail.com>
2024-10-19 10:15:49 +0530

    Commit

---
+++ app.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+import tensorflow as tf
+from pydantic import BaseModel
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+
+
+
+
+# Initialize FastAPI app
+app = FastAPI()
+
+# Load the saved model
+classify_model = tf.keras.models.load_model("neural_network_model_classification.h5")
+regression_model = tf.keras.models.load_model("regression.h5")
+
+# Define input data structure
+class ModelInput(BaseModel):
+    input_data: list  # Expecting a list of 34 values from the user
+
+# Endpoint for model prediction
+@app.post("/predict/")
+async def predict(input: ModelInput):
+    # Convert input data to numpy array and reshape it to match model's input shape
+    input_data = np.array(input.input_data).reshape(1, -1)  # Reshape for a single prediction
+    
+    # Scale the input data (use the same scaler used during training)
+    # input_data_scaled = scaler.transform(input_data)  # Uncomment if you're using a saved scaler
+    
+    # Get model predictions
+    prediction_prob = classify_model.predict(input_data)
+    # Convert probabilities to binary predictions (assuming binary classification)
+    prediction = (prediction_prob > 0.5).astype("int32").tolist()
+
+
+    return {"prediction": prediction}
